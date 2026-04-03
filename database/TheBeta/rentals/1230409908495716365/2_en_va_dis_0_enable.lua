1fd01492b11f280913e388922783cd6792f79289
ngtrhai3-lgtm <ngtrhai3@gmail.com>
2026-04-03 23:20:42 +0700

    Delete database/TheBeta directory

--- database/TheBeta/rentals/1230409908495716365/2_en_va_dis_0_enable.lua
+++
@@ -1,3 +0,0 @@
-local expiry = 1775319271
-if os.time() > expiry then print('Script het han!') return end
-loadstring(getInternet().getURL('https://raw.githubusercontent.com/ngtrhai3-lgtm/MyScripts/main/database/TheBeta/scripts/2_en_va_dis_0_enable.lua'))()
--- database/TheBeta/rentals/1230409908495716365/reg_3_enable.lua
+++
@@ -1,3 +0,0 @@
-local expiry = 1775319271
-if os.time() > expiry then print('Script het han!') return end
-loadstring(getInternet().getURL('https://raw.githubusercontent.com/ngtrhai3-lgtm/MyScripts/main/database/TheBeta/scripts/reg_3_enable.lua'))()
