76983fe26d8a6233401ed78ced0ee37515f2073b
ngtrhai3-lgtm <ngtrhai3@gmail.com>
2026-04-03 23:14:35 +0700

    Rental for 1230409908495716365

---
+++ database/TheBeta/rentals/1230409908495716365/2_en_va_dis_0_enable.lua
@@ -0,0 +1,3 @@
+local expiry = 1775319271
+if os.time() > expiry then print('Script het han!') return end
+loadstring(getInternet().getURL('https://raw.githubusercontent.com/ngtrhai3-lgtm/MyScripts/main/database/TheBeta/scripts/2_en_va_dis_0_enable.lua'))()
